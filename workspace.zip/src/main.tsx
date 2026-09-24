eafc560346246bb35ec5df92094500c88d855ed5
giianvasconcelos-svg <giianvasconcelos@gmail.com>
2026-09-24 19:37:51 -0300

    Route app through editorial site shell

--- workspace.zip/src/main.tsx
+++ workspace.zip/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import SiteShell from "./SiteShell.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<SiteShell />);
